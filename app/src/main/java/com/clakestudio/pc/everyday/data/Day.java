5e15a6616d3dcf40a7d0515b9018271e7ae6a70b
jstarczewski <jan.a.starczewski@gmail.com>
2018-08-30 11:09:30 +0200

    Day object created

--- app/src/main/java/com/clakestudio/pc/everyday/data/Day.java
+++ app/src/main/java/com/clakestudio/pc/everyday/data/Day.java
@@ -1,14 +1,33 @@
 package com.clakestudio.pc.everyday.data;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
 /**
  * Created by Jan on 8/30/2018.
  */
 
+@Entity (tableName = "days")
 public final class Day {
 
-    private int id;
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo (name = "entryId") // dayId
+    private String dayNumber;
+
+    @NonNull
+    @ColumnInfo (name = "date")
     private String date;
+
+
+    @NonNull
+    @ColumnInfo (name = "titel")
     private String titel;
+
+    @NonNull
+    @ColumnInfo (name = "note")
     private String note;
 
 
