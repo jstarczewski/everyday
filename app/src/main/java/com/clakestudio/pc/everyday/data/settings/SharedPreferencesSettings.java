5c46aa4e63fcdd662ab5e1c8ce14f7ccab01b23c
jstarczewski <jan.a.starczewski@gmail.com>
2018-09-06 18:39:44 +0200

    Basic SharedPreferencesSettings class created

--- app/src/main/java/com/clakestudio/pc/everyday/data/settings/SharedPreferencesSettings.java
+++ app/src/main/java/com/clakestudio/pc/everyday/data/settings/SharedPreferencesSettings.java
@@ -1,11 +1,35 @@
 package com.clakestudio.pc.everyday.data.settings;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by Jan on 9/6/2018.
  */
 
 public class SharedPreferencesSettings {
 
+    /**
+     * Could have been an dependency inejction but passing only a Context makes it easier to store data
+     * This gonna be an singleton ->
+     *                                 -> lets whether it works :0
+     * The cons are problems with testing
+     *
+     * */
 
+    private Context context;
+    private SharedPreferencesSettings sharedPreferencesSettings;
+    private SharedPreferences sharedPreferences;
+
+    public SharedPreferencesSettings getInstance(Context context) {
+        if (sharedPreferencesSettings==null) {
+            sharedPreferencesSettings = new SharedPreferencesSettings(context);
+        }
+        return sharedPreferencesSettings;
+    }
+    private SharedPreferencesSettings(Context context) {
+        this.context = context;
+        this.sharedPreferences = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+    }
 
 }
