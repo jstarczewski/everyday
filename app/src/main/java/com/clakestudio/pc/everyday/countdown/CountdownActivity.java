34291c067e4e50ba0ed5817bc3d297e9aeb10b83
jstarczewski <jan.a.starczewski@gmail.com>
2018-09-10 14:20:58 +0200

    CountdownActivity package and files created

---
+++ app/src/main/java/com/clakestudio/pc/everyday/countdown/CountdownActivity.java
@@ -0,0 +1,15 @@
+package com.clakestudio.pc.everyday.countdown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.clakestudio.pc.everyday.R;
+
+public class CountdownActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countdown);
+    }
+}
---
+++ app/src/main/java/com/clakestudio/pc/everyday/countdown/CountdownContract.java
@@ -0,0 +1,8 @@
+package com.clakestudio.pc.everyday.countdown;
+
+/**
+ * Created by Jan on 9/10/2018.
+ */
+
+public class CountdownContract {
+}
