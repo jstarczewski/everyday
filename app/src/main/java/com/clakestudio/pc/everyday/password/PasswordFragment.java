d97b318364bcc9f2930fbeadd674a7eef639eceb
jstarczewski <jan.a.starczewski@gmail.com>
2018-09-13 14:49:53 +0200

    Cleaning PasswordFragment

--- app/src/main/java/com/clakestudio/pc/everyday/password/PasswordFragment.java
+++ app/src/main/java/com/clakestudio/pc/everyday/password/PasswordFragment.java
@@ -2,8 +2,8 @@ package com.clakestudio.pc.everyday.password;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -16,22 +16,12 @@ import android.widget.Toast;
 
 import com.clakestudio.pc.everyday.R;
 import com.clakestudio.pc.everyday.forgotpassword.ForgotPasswordActivity;
-import com.clakestudio.pc.everyday.showdays.ShowDaysActivity;
 import com.clakestudio.pc.everyday.showgoal.ShowGoalActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PasswordFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PasswordFragment extends Fragment implements PasswordContract.View, View.OnClickListener, TextWatcher {
 
     private PasswordPresenter passwordPresenter;
     private EditText etPassword;
-    private Button btForgotPassword;
 
     public PasswordFragment() {
         // Required empty public constructor
@@ -54,11 +44,11 @@ public class PasswordFragment extends Fragment implements PasswordContract.View,
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_password, container, false);
-        btForgotPassword = (Button) view.findViewById(R.id.btForgotPassword);
-        etPassword = (EditText) view.findViewById(R.id.etPassword);
+        Button btForgotPassword = view.findViewById(R.id.btForgotPassword);
+        etPassword = view.findViewById(R.id.etPassword);
         etPassword.addTextChangedListener(this);
 
         btForgotPassword.setOnClickListener(this);
@@ -93,13 +83,15 @@ public class PasswordFragment extends Fragment implements PasswordContract.View,
     @Override
     public void showShowGoalActivity() {
         startActivity(new Intent(getActivity(), ShowGoalActivity.class));
-        getActivity().finish();
+        if (getActivity() != null)
+            getActivity().finish();
     }
 
     @Override
     public void showForgotPasswordActivity() {
         startActivity(new Intent(getActivity(), ForgotPasswordActivity.class));
-        getActivity().finish();
+        if (getActivity() != null)
+            getActivity().finish();
     }
 
     @Override
@@ -119,21 +111,7 @@ public class PasswordFragment extends Fragment implements PasswordContract.View,
 
     @Override
     public void afterTextChanged(Editable s) {
-            passwordPresenter.checkPasswordCorrectness(etPassword.getText().toString());
+        passwordPresenter.checkPasswordCorrectness(etPassword.getText().toString());
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
