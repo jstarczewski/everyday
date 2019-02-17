8e7523cf818f92d67034ae6f10db3ac0d55a746c
jstarczewski <jan.a.starczewski@gmail.com>
2019-02-17 13:03:02 +0100

    Building notification channel

--- app/src/main/java/com/clakestudio/pc/everyday/reminder/ReminderReceiver.java
+++ app/src/main/java/com/clakestudio/pc/everyday/reminder/ReminderReceiver.java
@@ -1,14 +1,11 @@
 package com.clakestudio.pc.everyday.reminder;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
 import com.clakestudio.pc.everyday.R;
 import com.clakestudio.pc.everyday.data.settings.SettingsRepository;
@@ -23,17 +20,28 @@ import com.clakestudio.pc.everyday.utils.SplashActivity;
 public class ReminderReceiver extends BroadcastReceiver {
 
     private static final String CHANNEL_ID = "REMINDER";
+    private static final int notificationId = 0;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
         SettingsRepository settingsRepository = SettingsRepository.getInstance(SharedPreferencesSettings.getInstance(context));
 
+        Intent notificationIntent = new Intent(context, SplashActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.logosplash)
                 .setContentTitle("Focus time")
-                .setContentText(textContent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setContentText("It is time to focus on your goal")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(notificationId, mBuilder.build());
+
 
         /*
         if (!settingsRepository.isReminderSet()) {
@@ -68,7 +76,6 @@ public class ReminderReceiver extends BroadcastReceiver {
     }
 
     private void createNotificationChanngel() {
-
 
 
     }
--- app/src/main/java/com/clakestudio/pc/everyday/utils/SplashActivity.java
+++ app/src/main/java/com/clakestudio/pc/everyday/utils/SplashActivity.java
@@ -1,10 +1,14 @@
 package com.clakestudio.pc.everyday.utils;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.clakestudio.pc.everyday.R;
 import com.clakestudio.pc.everyday.data.settings.SettingsRepository;
 import com.clakestudio.pc.everyday.data.settings.SharedPreferencesSettings;
 import com.clakestudio.pc.everyday.info.InfoActivity;
@@ -20,18 +24,34 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         SettingsRepository settingsRepository = SettingsRepository.getInstance(SharedPreferencesSettings.getInstance(this));
 
         if (!settingsRepository.isInfoRead()) {
             startActivity(new Intent(this, InfoActivity.class));
         } else if (!settingsRepository.isGoalSet()) {
             startActivity(new Intent(this, SetGoalActivity.class));
-        }
-        else if(settingsRepository.isPasswordSet()) {
+        } else if (settingsRepository.isPasswordSet()) {
             startActivity(new Intent(this, PasswordActivity.class));
         } else {
             startActivity(new Intent(this, ShowGoalActivity.class));
         }
         finish();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "REMINDER";
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
